ca75fab69286c9456206c153ecb03095e946205c
etaoxing <etaoxing@users.noreply.github.com>
2022-04-13 00:29:43 -0400

    Separete feature_extractor and policy from `ImpalaNet`

--- multibeast/agents/impala/impalanet.py
+++ multibeast/agents/impala/impalanet.py
@@ -1,113 +1,92 @@
 # modified from https://github.com/facebookresearch/moolib/blob/e8b2de7ac5df3a9b3ee2548a33f61100a95152ef/examples/atari/models.py
 
 # Copyright (c) Facebook, Inc. and its affiliates.
+from typing import Dict
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from moolib.examples.common import nest
+from tinyspace import Space, sample_from_space
+
+from multibeast.builder import __PolicyNet__
+
+from .policynet import get_action_params
 
 
-class Net(nn.Module):
-    def __init__(self, num_actions=18, input_channels=4, use_lstm=False):
-        super(Net, self).__init__()
-        self.num_actions = num_actions
+class ImpalaNet(nn.Module):
+    def __init__(
+        self,
+        observation_space: Space,
+        action_space: Space,
+        action_dist_params: dict = None,
+        policy_params: Dict = dict(cls="PolicyNet"),  # noqa: B006
+        feature_extractor: nn.Module = None,
+        use_lstm: bool = False,
+        fc_out_features: int = 256,
+    ):
+        super().__init__()
+        self.observation_space = observation_space
+        self.action_space = action_space
+
+        self.num_actions, self.max_action, self.action_dist_params = get_action_params(
+            action_space,
+            action_dist_params,
+        )
+
+        self.feature_extractor = feature_extractor
         self.use_lstm = use_lstm
 
-        self.feat_convs = []
-        self.resnet1 = []
-        self.resnet2 = []
+        if feature_extractor:
+            with torch.no_grad():
+                x = sample_from_space(self.observation_space, to_torch_tensor=True)
+                feature_dim = tuple(self.feature_extractor(x).squeeze(0).shape)[0]
+            self.fc = nn.Sequential(nn.ReLU(inplace=True), nn.Linear(feature_dim, fc_out_features), nn.ReLU(inplace=True))
+        else:
+            feature_dim = observation_space["shape"][0]
+            self.fc = nn.Sequential(nn.Linear(feature_dim, fc_out_features), nn.ReLU(inplace=True))
 
-        self.convs = []
-
-        for num_ch in [16, 32, 32]:
-            feats_convs = []
-            feats_convs.append(
-                nn.Conv2d(
-                    in_channels=input_channels,
-                    out_channels=num_ch,
-                    kernel_size=3,
-                    stride=1,
-                    padding=1,
-                )
-            )
-            feats_convs.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
-            self.feat_convs.append(nn.Sequential(*feats_convs))
-
-            input_channels = num_ch
-
-            for i in range(2):
-                resnet_block = []
-                resnet_block.append(nn.ReLU())
-                resnet_block.append(
-                    nn.Conv2d(
-                        in_channels=input_channels,
-                        out_channels=num_ch,
-                        kernel_size=3,
-                        stride=1,
-                        padding=1,
-                    )
-                )
-                resnet_block.append(nn.ReLU())
-                resnet_block.append(
-                    nn.Conv2d(
-                        in_channels=input_channels,
-                        out_channels=num_ch,
-                        kernel_size=3,
-                        stride=1,
-                        padding=1,
-                    )
-                )
-                if i == 0:
-                    self.resnet1.append(nn.Sequential(*resnet_block))
-                else:
-                    self.resnet2.append(nn.Sequential(*resnet_block))
-
-        self.feat_convs = nn.ModuleList(self.feat_convs)
-        self.resnet1 = nn.ModuleList(self.resnet1)
-        self.resnet2 = nn.ModuleList(self.resnet2)
-
-        self.fc = nn.Linear(3872, 256)
-
-        # FC output size + one-hot of last action + last reward.
-        core_output_size = self.fc.out_features + num_actions + 1
+        # FC output size + last action (one-hot if discrete) + last reward.
+        core_output_size = fc_out_features + self.num_actions + 1
 
         if use_lstm:
             self.core = nn.LSTM(core_output_size, 256, num_layers=1)
             core_output_size = 256
 
-        self.policy = nn.Linear(core_output_size, self.num_actions)
+        self.policy = __PolicyNet__.build(policy_params, core_output_size, self.num_actions, self.action_dist_params)
         self.baseline = nn.Linear(core_output_size, 1)
 
     def initial_state(self, batch_size=1):
-        if not self.use_lstm:
-            return tuple()
-        return tuple(torch.zeros(self.core.num_layers, batch_size, self.core.hidden_size) for _ in range(2))
+        if self.use_lstm:
+            return tuple(torch.zeros(self.core.num_layers, batch_size, self.core.hidden_size) for _ in range(2))
+        return tuple()
 
-    def forward(self, inputs, core_state=None):
+    def forward(self, inputs, core_state=None, deterministic: bool = False):
+        prev_action = inputs["prev_action"]
         reward = inputs["reward"]
         x = inputs["state"]
 
-        T, B, *_ = x.shape
-        x = torch.flatten(x, 0, 1)  # Merge time and batch.
-        x = x.float() / 255.0
+        T, B = reward.shape
+        if self.feature_extractor:
+            x = self.feature_extractor(x)
+            if len(x.shape) != 2:
+                raise RuntimeError(f"Expected feature extractor to output batch of 1d features, got {x.shape}")
 
-        res_input = None
-        for i, fconv in enumerate(self.feat_convs):
-            x = fconv(x)
-            res_input = x
-            x = self.resnet1[i](x)
-            x += res_input
-            res_input = x
-            x = self.resnet2[i](x)
-            x += res_input
-
-        x = F.relu(x)
         x = x.view(T * B, -1)
-        x = F.relu(self.fc(x))
+        x = self.fc(x)
 
-        one_hot_last_action = F.one_hot(inputs["prev_action"].view(T * B), self.num_actions).float()
         clipped_reward = torch.clamp(reward, -1, 1).view(T * B, 1)
-        core_input = torch.cat([x, clipped_reward, one_hot_last_action], dim=-1)
+        if self.action_space["cls"] == "discrete":
+            if prev_action.dtype != torch.long:
+                # if getting "RuntimeError: one_hot is only applicable to index tensor."
+                # then prev_action is not a long
+                raise RuntimeError
+            last_action = F.one_hot(prev_action.view(T * B), self.num_actions).float()
+        elif self.action_space["cls"] == "box":
+            last_action = prev_action.view(T * B, -1)
+        else:
+            raise ValueError
+        core_input = torch.cat([x, clipped_reward, last_action], dim=-1)
 
         if self.use_lstm:
             done = inputs["done"]
@@ -120,30 +99,20 @@ class Net(nn.Module):
                 # states:
                 nd = nd.view(1, -1, 1)
                 core_state = nest.map(nd.mul, core_state)
-                output, core_state = self.core(input.unsqueeze(0), core_state)
-                core_output_list.append(output)
+                core_output, core_state = self.core(input.unsqueeze(0), core_state)
+                core_output_list.append(core_output)
             core_output = torch.flatten(torch.cat(core_output_list), 0, 1)
         else:
             core_output = core_input
 
-        policy_logits = self.policy(core_output)
+        core_output = core_output.view(T, B, -1)
+        output = self.policy(core_output, deterministic=deterministic)
+        if self.action_space["cls"] == "discrete":
+            output["action"] = output["action"].view(T, B)
+
         baseline = self.baseline(core_output)
-
-        action = torch.multinomial(F.softmax(policy_logits, dim=1), num_samples=1)
-
-        policy_logits = policy_logits.view(T, B, self.num_actions)
         baseline = baseline.view(T, B)
-        action = action.view(T, B)
-
-        output = dict(
-            policy_logits=policy_logits,
+        output.update(
             baseline=baseline,
-            action=action,
         )
         return output, core_state
-
-
-def create_model(flags):
-    model = Net(use_lstm=flags.use_lstm)
-    model.to(device=flags.device)
-    return model
---
+++ multibeast/agents/impala/policynet.py
@@ -0,0 +1,153 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from multibeast.builder import __Distribution__, __PolicyNet__
+
+
+def get_action_params(action_space, action_dist_params):
+    if action_space["cls"] == "discrete":
+        num_actions = action_space["high"]
+        max_action = num_actions
+        if action_dist_params is None:
+            action_dist_params = dict(cls="Categorical")  # same as `torch.multinomial`
+    elif action_space["cls"] == "box":
+        num_actions = action_space["shape"][0]
+        try:
+            max_action = max(action_space["high"])
+        except:  # TypeError: 'float' object is not iterable
+            max_action = action_space["high"]
+        if action_dist_params is None:
+            action_dist_params = dict(cls="TanhNormal")
+    else:
+        raise ValueError
+    return num_actions, max_action, action_dist_params
+
+
+@__PolicyNet__.register()
+class PolicyNet(nn.Module):
+    def __init__(
+        self,
+        core_output_size,
+        num_actions,
+        action_dist_params,
+        learn_std=None,
+        state_dependent_std=False,
+        init_std=1.0,
+        min_std=1e-6,
+        max_std=None,
+    ):
+        super().__init__()
+        self.logits = nn.Linear(core_output_size, num_actions)
+        self.action_dist_cls = __Distribution__.build(action_dist_params)
+
+        if learn_std is not None:
+            output_dim = num_actions
+
+            if state_dependent_std:
+                self._log_std = nn.Linear(core_output_size, output_dim)
+            else:
+                self._init_std = torch.Tensor([init_std]).log()
+                log_std = torch.Tensor([init_std] * output_dim).log()
+                if learn_std:
+                    self._log_std = torch.nn.Parameter(log_std)
+                else:
+                    self._log_std = log_std
+                    self.register_buffer("log_std", self._log_std)
+
+            raise NotImplementedError
+
+    def action_dist(self, policy_logits):
+        return self.action_dist_cls(logits=policy_logits)
+
+    def forward(self, core_output, deterministic: bool = False):
+        T, B, _ = core_output.shape
+
+        policy_logits = self.logits(core_output.view(T * B, -1))
+
+        action_dist = self.action_dist(policy_logits)
+        if deterministic:
+            raise NotImplementedError
+        else:
+            action = action_dist.sample()
+
+        action = action.view(T, B, -1)
+        policy_logits = policy_logits.view(T, B, -1)
+
+        return dict(policy_logits=policy_logits, action=action)
+
+
+# from https://github.com/SudeepDasari/one_shot_transformers/blob/ecd43b0c182451b67219fdbc7d6a3cd912395f17/hem/models/inverse_module.py#L106
+# They report "For most of our experiments, the model performed best when using two mixture
+# components and learned constant variance parameters per action dimension", so `n_mixtures=2` and `const_var=True`.
+# Also see https://github.com/ikostrikov/jaxrl/blob/8ac614b0c5202acb7bb62cdb1b082b00f257b08c/jaxrl/networks/policies.py#L47
+@__PolicyNet__.register()
+class MixturePolicyNet(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        action_dist_params,
+        n_mixtures=3,
+        const_var=False,
+        hidden_dim=256,
+    ):
+        super().__init__()
+        self.action_dist_cls = __Distribution__.build(action_dist_params)
+
+        assert n_mixtures >= 1, "must predict at least one mixture!"
+        self._n_mixtures, self._dist_size = n_mixtures, torch.Size((out_dim, n_mixtures))
+
+        if hidden_dim:
+            self._l = nn.Sequential(
+                nn.Linear(in_dim, hidden_dim),
+                nn.ReLU(inplace=True),
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            self._l = None
+            hidden_dim = in_dim
+
+        self._mu = nn.Linear(hidden_dim, out_dim * n_mixtures)
+        self._const_var = const_var
+
+        if const_var:
+            # independent of state, still optimized
+            ln_scale = torch.randn(out_dim, dtype=torch.float32) / np.sqrt(out_dim)
+            self.register_parameter("_ln_scale", nn.Parameter(ln_scale, requires_grad=True))
+        else:
+            # state dependent
+            self._ln_scale = nn.Linear(hidden_dim, out_dim * n_mixtures)
+        self._logit_prob = nn.Linear(hidden_dim, out_dim * n_mixtures) if n_mixtures > 1 else None
+
+    def action_dist(self, policy_logits_tuple):
+        return self.action_dist_cls(*policy_logits_tuple)
+
+    def forward(self, core_output, deterministic: bool = False):
+        T, B, _ = core_output.shape
+
+        x = core_output.view(T * B, -1)
+        if self._l is not None:
+            x = self._l(x)
+
+        mu = self._mu(x).reshape((x.shape[:-1] + self._dist_size))
+        if self._const_var:
+            ln_scale = self._ln_scale if self.training else self._ln_scale.detach()
+            ln_scale = ln_scale.reshape((1, 1, -1, 1)).expand_as(mu)
+        else:
+            ln_scale = self._ln_scale(x).reshape((x.shape[:-1] + self._dist_size))
+
+        logit_prob = (
+            self._logit_prob(x).reshape((x.shape[:-1] + self._dist_size)) if self._n_mixtures > 1 else torch.ones_like(mu)
+        )
+
+        policy_logits_tuple = (mu, ln_scale, logit_prob)
+
+        action_dist = self.action_dist(policy_logits_tuple)
+        if deterministic:
+            raise NotImplementedError
+        else:
+            action = action_dist.sample()
+
+        return dict(policy_logits=policy_logits_tuple, action=action)
